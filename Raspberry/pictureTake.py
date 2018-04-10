3a1ee085349bd51335863f7b40835d9061903d09
KeitHLeE94 <wogus4187@naver.com>
2018-04-10 18:29:00 +0900

    ssh로 연결하여 사진촬영 가능

---
+++ Raspberry/pictureTake.py
@@ -0,0 +1,17 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+ssh.connect('192.168.43.55', username='pi', password='raspberry')
+#라즈베리파이 ssh 접속
+
+stdin, stdout, stderr = ssh.exec_command('raspistill -o sample.jpg')
+#사진 촬영 명령 실행
+for line in stdout:
+    print('this is stdout')
+    print('...' + line.strip('\n'))
+for line in stderr:
+    print('this is stderr')
+    print('...' + line.strip('\n'))
+#사진 촬영 실패시 출력
+ssh.close()
